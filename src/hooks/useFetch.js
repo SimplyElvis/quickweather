9de220493c1e3b1e6dc98514e74ceba372b7182b
Simply Elvis <provictor.ie@gmail.com>
2021-11-11 01:24:55 +0100

    Fetch api hook.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,3 +1,24 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const useFetch = (url, options) => {};
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((data) => {
+        setData(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [url]);
+
+  return { data, loading };
+};
+
+export default useFetch;
